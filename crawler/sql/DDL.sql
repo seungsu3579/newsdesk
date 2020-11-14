6820042c42bf99b3de68cde7cf4b179c629e1b07
AN SE HYEON <sehyeon.an@voithru.com>
2020-11-14 19:35:56 +0900

    fix: add news_collecting_log ddl

--- crawler/sql/DDL.sql
+++ crawler/sql/DDL.sql
@@ -11,4 +11,17 @@ CREATE TABLE news_collecting_log
     representative                  BOOLEAN      DEFAULT FALSE,
     download_date                   TIMESTAMP,
     update_date                     TIMESTAMP DEFAULT now(),
-    PRIMARY KEY (created_date, category, article_url));
+    PRIMARY KEY (created_date, category, article_url)
+    );
+
+-- new desk crawling log
+CREATE TABLE news_collecting_log
+(
+    news_id                         BIGINT,
+    retrieve_datetime               TIMESTAMP,
+    download_datetime               TIMESTAMP,
+    update_datetime                 TIMESTAMP DEFAULT now(),
+    download_execution_time         TIME,
+    error_message                   TEXT
+    PRIMARY KEY (created_date, category, article_url)
+    );
