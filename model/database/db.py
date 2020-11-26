a2d10595c96dfdb06a629a6d6b25a1a6e6f2ab4c
seungsu3579 <seungsu3579@gmail.com>
2020-11-26 16:44:15 +0900

    [feat] skelton code of db

--- model/database/db.py
+++ model/database/db.py
@@ -1,2 +1,28 @@
 from .connection import *
 
+
+def insert_keyword_keysentence(news_id, key_sentence, keyword):
+    """
+    input :
+        news_id : 뉴스 아이디 
+        key_sentence : 중요 문장 (varchar)
+        keyword : 중요 단어 (varchar)
+    return :
+        boolean
+    function :
+        news_extracted 테이블에 분석된 키워드를 삽입
+    """
+    pass
+
+
+def is_analysis(news_id):
+    """
+    input :
+        news_id : 뉴스 아이디 
+    return :
+        boolean
+    function :
+        이 뉴스가 키워드 분석이 되었는지 여부
+    """
+
+    pass
