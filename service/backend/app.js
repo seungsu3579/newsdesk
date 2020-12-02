2c8f47e0d7edb9b39e068ea55ce3d5edbe94d949
JungYul Yang <passionyang16@gmail.com>
2020-12-02 08:34:01 +0000

    [chores] gitignore update

--- service/backend/app.js
+++ service/backend/app.js
@@ -13,6 +13,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//port setup
+app.set('port', process.env.PORT || 3000)
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -39,3 +42,9 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+var server = app.listen(app.get('port'), function() {
+
+  console.log('Express server listening on port ' + server.address().port);
+  
+  });
